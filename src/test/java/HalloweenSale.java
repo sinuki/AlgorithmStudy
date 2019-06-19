948dc6a70a0071667a7eefe0066e832ff2797606
sinuki <parksw7302@gmail.com>
2019-06-19 21:45:14 +0900

    HalloweenSale

---
+++ src/test/java/HalloweenSale.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * https://www.hackerrank.com/challenges/halloween-sale/problem?isFullScreen=true
+ */
+public class HalloweenSale {
+
+    private int howManyGames(int p, int d, int m, int s) {
+        int count = 0;
+
+        while (p <= s) {
+            s -= p;
+            p = Math.max(p - d, m);
+            count++;
+        }
+
+        return count;
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(6, howManyGames(20, 3, 6, 80));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(7, howManyGames(20, 3, 6, 85));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(9917, howManyGames(16, 2, 1, 9981));
+    }
+}
