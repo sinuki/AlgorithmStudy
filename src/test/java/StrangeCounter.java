95fa2354d37ea6c3694ad0f9d604636687c8d4f7
sinuki <parksw7302@gmail.com>
2019-07-13 22:10:05 +0900

    StrangeCounter

---
+++ src/test/java/StrangeCounter.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * https://www.hackerrank.com/challenges/strange-code/problem?isFullScreen=true
+ */
+public class StrangeCounter {
+
+    private long strangeCounter(long t) {
+        long init = 3L;
+        while (t > init) {
+            t -= init;
+            init *= 2;
+        }
+        return init - t + 1;
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(6L, strangeCounter(4L));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(5L, strangeCounter(17L));
+    }
+}
