1c3d57fbfdcef131ac681b60de602c3b94b8e17d
sinuki <parksw7302@gmail.com>
2019-08-11 23:27:20 +0900

    BirthdayCakeCandle

---
+++ src/test/java/easy/BirthdayCakeCandle.java
@@ -0,0 +1,40 @@
+package easy;
+
+import org.junit.Test;
+
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
+ **/
+public class BirthdayCakeCandle {
+
+    private int birthdayCakeCandles(int[] ar) {
+        return IntStream.of(ar)
+                        .boxed()
+                        .collect(groupingBy(Function.identity(), counting()))
+                        .entrySet()
+                        .stream()
+                        .sorted((entry1, entry2) -> {
+                            int keyOrder = entry1.getKey().compareTo(entry2.getKey()) * - 1;
+                            int valueOrder = entry1.getValue().compareTo(entry2.getValue()) * - 1;
+
+                            if (valueOrder == 0)    return keyOrder;
+                            return valueOrder;
+                        })
+                        .findFirst()
+                        .get()
+                        .getValue()
+                        .intValue();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(2, birthdayCakeCandles(new int[] {3, 2, 1, 3}));
+    }
+}
